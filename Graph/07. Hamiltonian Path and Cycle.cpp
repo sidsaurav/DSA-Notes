34632b7acc4a8a6eef3ac2ab33d7f5dbb1735840
Siddharth Saurav <sid.saurav11@gmail.com>
2022-07-10 21:10:05 +0530

    edit

--- Graph/07. Hamiltonian Path and Cycle.cpp
+++ Graph/07. Hamiltonian Path and Cycle.cpp
@@ -1,9 +1,53 @@
-//A mother vertex in a graph G = (V, E) is a vertex v such that all other 
-//vertices in G can be reached by a path from v.
+#include <bits/stdc++.h>
+using namespace std;
 
 
-do topo sort by dfs. 
-first node in toposort is the mv if mother vertex exists. 
-proof - agr first node ni h aur uske niche wale nodes me se koi h (visualise stack of topo sort) 
-to us node se first node pe ek na ek rasta hoga and aesa h to first 
-node kavi top pe ni rahega which is false. so agr h to first wala node hi h.
+vvi g;
+vi vis;
+int n, e;
+
+void dfs(int node, string str, int cnt){
+    //
+    if(cnt == n){
+        cout << str;
+        bool find = false;
+        for(auto x : g[node]){
+            if(x == 0)
+            find = true;
+        }
+        if(find) cout << "*";
+        else cout << ".";
+        cout << endl;
+        return;
+    }
+
+    //
+    vis[node] = 1;
+    for(auto x : g[node]){
+        if(!vis[x]){
+            dfs(x, str + to_string(x), cnt+1);
+        }
+    }
+    vis[node] = 0;
+}
+
+void solve(int case_num){
+    cin >> n >> e;
+    g.resize(n+1);
+    vis.resize(n+1);
+    for(int i = 0; i<e; i++){
+        int a, b, wt;
+        cin >> a >> b >> wt;
+        g[a].pb(b);
+        g[b].pb(a);
+    }
+    dfs(0, "0", 1);
+}
+
+int32_t main(){
+    fastio();
+    int t = 1;
+    //cin >> t;
+    rep(i, 1, t+1)
+    solve(i);
+}    
--- Graph/08. Eulerian Path and Circuit.cpp
+++ Graph/08. Eulerian Path and Circuit.cpp
@@ -1,110 +1,22 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-#define ff first
-#define ss second
-#define nl endl
-
-int n;
-int k;
-int num;
-vector<vector<pair<int, int>>> g;
-int src;
-int dest;
-vector<int> vis;
-
-string smallest;
-int smallest_wt = INT_MAX;
-
-string largest;
-int largest_wt = INT_MIN;
-
-string just_larger;
-int just_larger_wt = INT_MAX;
-
-string just_smaller;
-int just_smaller_wt = INT_MIN;
-
-set<pair<int, string>> s;
-
-void dfs(int src, int sum, string path){
-    //
-    if(src == dest){
-        if(smallest_wt>sum){
-            smallest = path;
-            smallest_wt = sum;
-        }
-
-        if(largest_wt<sum){
-            largest = path;
-            largest_wt = sum;
-        }
-        if(sum>num and sum<just_larger_wt){
-            just_larger_wt = sum;
-            just_larger = path;
-        }
+Eulerian Path -> Path which visits every edge once and only once.
+Eulerian Circuit -> Eulerian Path whose start and end nodes are same.
+ 
+Agr eulerian circuit h to wo path v h.
+ 
+Undirected Graph :-
+ 
+E Circuit -> every node degree is even
+E Path -> Atmost 2 node degree odd else every node degree even
+// Logic -> agr degree even hoga to jane ka and aane ka rasta hoga sare nodes se. for path agr degree odd h to wo src ya dest banega.
+ 
+Directed Graph :-
+ 
+E Circuit -> indegree == outdegree for every node
+E path -> One node may have indegree == out + 1 and one have outdegree = in + 1
+ 
+To find Eulerian Path for directed graph : https://leetcode.com/problems/reconstruct-itinerary/
 
 
-        if(sum<num and sum>just_smaller_wt){
-            just_smaller_wt = sum;
-            just_smaller = path;
-        }
 
-        s.insert({sum, path});
-        return;
-    }
 
-    //
-    vis[src] = 1;
-    for(auto x : g[src]){
-        if(vis[x.ff] == 0){
-            dfs(x.ff, sum+x.ss, path+"-"+to_string(x.ff));
-        }
-    }
-    vis[src] = 0;
-}
-
-void solve(){
-    cin >> n;
-    g.resize(n+1);
-    vis.resize(n+1);
-    int e;
-    cin >> e;
-    for(int i = 0; i<e; i++){
-        int a, b, wt;
-        cin >> a >> b >> wt;
-        g[a].push_back({b, wt});
-        g[b].push_back({a, wt});
-    }
-    cin >> src;
-    cin >> dest;
-    cin >> num;
-    cin >> k;
-
-    dfs(src, 0, to_string(src));
-
-    cout << smallest_wt << nl;
-    cout << smallest << nl << nl;
-    
-    cout << largest_wt << nl;
-    cout << largest << nl << nl;
-
-    cout << just_larger_wt << nl;
-    cout << just_larger << nl<<nl;
-
-    cout << just_smaller_wt << nl;
-    cout << just_smaller << nl << nl;
-
-    auto it = s.rbegin();
-    for(int i = 0; i<k-1; i++){
-        it++;
-    }
-
-    // deb(s);
-    cout << (*it).ff << nl;
-    cout << (*it).ss << nl;
-}
-
-int32_t main(){
-    solve();
-}    
+//https://www.youtube.com/watch?v=w99el8bl-DM
